522a285493bb5b6a90c2a0503070ba21c2ed30a8
Ajmal0721 <169044559+Ajmal0721@users.noreply.github.com>
2024-09-10 08:55:09 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Compo from "./components/compo"
+
+function App() {
+
+  return (
+    <>
+      <h1>hello? world Why</h1>
+      <Compo />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Compo.jsx
@@ -0,0 +1,8 @@
+function Compo()
+{
+    return (
+        <h1>Red & White Multimedia</h1>
+    )
+}
+
+export default Compo
